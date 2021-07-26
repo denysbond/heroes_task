71f9a3ce7ae004b19624b7bcde1c5ddfdf999cec
Sergi Bilous <bilous70000@gmail.com>
2021-07-26 16:28:50 +0300

    quick fix of packages

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");xs
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   // entry: "./src/index.js",
