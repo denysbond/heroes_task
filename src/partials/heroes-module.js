491982bf7cc0f133b0567acdd3774a76927c6792
Vitalii Satanovskyi <satanovskiy.vitaliy95@gmail.com>
2021-07-26 13:00:01 +0300

    Was added readme

--- src/partials/heroes-module.js
+++ src/partials/heroes-module.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
-import Heroes from "./heroes.handlebars";
+import HeroesList from "./heroes-list.handlebars";
 const url = "https://api.opendota.com";
 
 (async () => {
   const heroes = await axios.get(`${url}/api/heroes`);
   const heroesDetails = await axios.get(`${url}/api/heroStats`);
-  const content = document.getElementById("content");
-  const list = document.createElement("ul");
-  heroes.data.forEach((_hero, index) => {
-    const hero = Heroes({ nameHero: _hero.localized_name, srcImg: `${url}${heroesDetails.data[index].img }`});
-    const li = document.createElement("li");
-    li.innerHTML = hero;
-    list.append(li);
+  const div = document.createElement("div");
+  div.innerHTML = HeroesList({
+    heroes: heroes.data.map((_hero, index) => ({ nameHero: _hero.localized_name, srcImg: `${url}${heroesDetails.data[index].img}` })),
   });
-  content.append(list);
+  // const content = document.getElementById("content");
+  // const list = document.createElement("ul");
+  // heroes.data.forEach((_hero, index) => {
+  //   const hero = Heroes({ nameHero: _hero.localized_name, srcImg: `${url}${heroesDetails.data[index].img }`});
+  //   const li = document.createElement("li");
+  //   li.innerHTML = hero;
+  //   list.append(li);
+  // });
+  content.append(div);
 })();
